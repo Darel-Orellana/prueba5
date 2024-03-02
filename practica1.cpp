3dd9a0dcd90c44ee900a78398ae72525d4838864
Andy Orellana <galeanoo@miumg.edu.gt>
2024-03-02 13:28:16 -0600

    Pureba

---
+++ practica1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   int numero;
+   int b= 0;
+cout<<"Ingrese un numero:";
+cin >> numero;
+
+    if (numero>b){
+        cout << "Este numero es positivo." <<endl;
+    }
+    else{
+        if (numero <b)
+        {
+            cout <<"Este numero es negativo.";
+        }
+
+        else{
+            if (numero==b) {
+                cout <<"Este numero es cero.";
+            }
+        }
+
+        }
+
+
+
+    return 0;
+}
